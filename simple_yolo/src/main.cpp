b9ded7ae931a6e0ae65db3d39c405558ee4c579b
PeterHuang <qsmy122011@gmail.com>
2021-11-05 04:44:48 +0000

    update comments of simple_yolo contribuded by zhiqingchun

--- simple_yolo/src/main.cpp
+++ simple_yolo/src/main.cpp
@@ -111,6 +111,7 @@ bool requires_model(const string& name) {
 
 static void inference_and_performance(int deviceid, const string& engine_file, SimpleYolo::Mode mode, SimpleYolo::Type type, const string& model_name){
 
+    /* @致青春  创建推理引擎，同时初始化处理线程 */
     auto engine = SimpleYolo::create_infer(engine_file, type, deviceid, 0.4f, 0.5f);
     if(engine == nullptr){
         printf("Engine is nullptr\n");
@@ -183,6 +184,7 @@ static void test(SimpleYolo::Type type, SimpleYolo::Mode mode, const string& mod
     const char* name = model.c_str();
     printf("===================== test %s %s %s ==================================\n", SimpleYolo::type_name(type), mode_name, name);
 
+    // @致青春 判断模型是否存在，不存在则远程下载
     if(!requires_model(name))
         return;
 
@@ -190,6 +192,7 @@ static void test(SimpleYolo::Type type, SimpleYolo::Mode mode, const string& mod
     string model_file = cv::format("%s_dynamic.%s.trtmodel", name, mode_name);
     int test_batch_size = 16;
     
+    // @致青春 判断是否导出trt模型，如果导出继续向下执行，反之进行导出操作
     if(!exists(model_file)){
         SimpleYolo::compile(
             mode, type,                 // FP32、FP16、INT8
@@ -200,6 +203,7 @@ static void test(SimpleYolo::Type type, SimpleYolo::Mode mode, const string& mod
             "inference"
         );
     }
+    // @致青春 模型导出后，则进行执行，此时的直线直接是反序列化即可读取模型
     inference_and_performance(deviceid, model_file, mode, type, name);
 }
 
